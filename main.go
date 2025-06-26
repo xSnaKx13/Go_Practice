c8c202e5a7fd59acc5c50fe564e26d8bc1e03b99
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-06-26 08:52:27 +0000

    test

--- main.go
+++ main.go
@@ -16,5 +16,6 @@ func main() {
          fmt.Print(elem, " ")
       }
    }
+   dude()
 }
    
--- practice.go
+++ practice.go
@@ -1,1 +1,17 @@
+package main
+import "fmt"
 
+func dude(){
+	var countNumber int
+	fmt.Scan(&countNumber)
+	var num int
+	var positiveNumber int
+
+	for i := 0; i < countNumber; i++ {
+		fmt.Scan(&num)
+		if num > 0{
+			positiveNumber++
+		}
+	}
+	fmt.Print(positiveNumber)
+}
