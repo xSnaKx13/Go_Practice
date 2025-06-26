00656a2e709166bf0d2127bf7669be924614b5cf
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-06-26 14:13:27 +0000

    practice

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
    var a,b,c int
    fmt.Scan(&a,&b,&c)
 
-   if a+b>c||a+c>b||b+c>a{
+   if a+b>c&&a+c>b&&b+c>a{
       fmt.Print("Существует")
    }else{
       fmt.Print("Не существует")
