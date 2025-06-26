dd2363c90a6f1fba5c2606d9f76f92049c422390
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-06-26 13:55:00 +0000

    practice

--- main.go
+++ main.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-   var variable int
-   fmt.Scan(&variable)
+   var a,b,c int
+   fmt.Scan(&a,&b,&c)
 
-   number1 := variable / 100
-   number2 := (variable / 10) % 10
-   number3 := variable % 10
-
-   numbers := fmt.Sprintf("%d%d%d", number3, number2, number1)
-   fmt.Print(numbers)
+   if a*a+b*b==c*c {
+      fmt.Print("Прямоугольный")
+   }else{
+      fmt.Print("Непрямоугольный")
+   }
 }
    
