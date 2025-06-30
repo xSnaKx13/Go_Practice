f64ffad428c07fda4547d3eee3406f446550eb43
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-06-30 10:20:25 +0000

    less

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-   var numbers []int
    var nCount int
    fmt.Scan(&nCount)
    zeroCount := 0
+
+   var numbers = make([]int, nCount)
+
    for i := 0; i < nCount; i++ {
       fmt.Scan(&numbers[i])
       if numbers[i] == 0 {
