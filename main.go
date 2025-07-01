e4890c97137329b333105e754494e5c4a297aaba
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-01 15:41:45 +0000

    less

--- main.go
+++ main.go
@@ -5,12 +5,6 @@ import "fmt"
 func main() {
    var numb int
    fmt.Scan(&numb)
-
-   square := 1
-
-   for square < numb{
-      fmt.Print(square, " ")
-      square *= 2
-   }
+   fmt.Printf("%b", numb)
 }
    
