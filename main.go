18f30d0eb4dc8a82a145dfde3720a87c0b0251a0
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-01 15:37:07 +0000

    less

--- main.go
+++ main.go
@@ -5,20 +5,12 @@ import "fmt"
 func main() {
    var numb int
    fmt.Scan(&numb)
-   if numb%100 >= 11 && numb%100 <=14{
-      fmt.Printf("%d korov", numb)
-      return
-   }
-   cowCount := numb % 10
 
-   switch cowCount{
-   case 1:
-      fmt.Printf("%d korova", numb)
-   case 2, 3, 4:
-      fmt.Printf("%d korovy", numb)
-   case 0, 5, 6, 7, 8, 9:
-      fmt.Printf("%d korov", numb)
-   }
+   square := 1
 
+   for square < numb{
+      fmt.Print(square, " ")
+      square *= 2
+   }
 }
    
