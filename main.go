1a640b6093c57279572ea8b7f8e94a56dd775a75
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-01 13:25:18 +0000

    lessResult

--- main.go
+++ main.go
@@ -3,22 +3,16 @@ package main
 import "fmt"
 
 func main() {
-   var number int
-   fmt.Scan(&number)
-   var testNum string
+   var firstNumber, secondNumber int
+   fmt.Scan(&firstNumber, &secondNumber)
 
-   for number > 0{
-      digit := number%10
-      if digit != 7{
-         testNum += fmt.Sprintf("%d", digit)
+   var result int
+
+   for i := firstNumber; i < secondNumber; i++ {
+      if i % 7 == 0{
+         result = i
       }
-      number /= 10
    }
-   fmt.Print(testNum, "\n")
-   var reverse string
-   for i := len(testNum)-1; i >=0; i-- {
-      reverse += string(testNum[i])
-   }
-   fmt.Print(reverse)
+   fmt.Print(result)
 }
    
