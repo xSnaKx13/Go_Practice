e5c4943e5d56cc507650430cc9d9830c1fc99175
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-02 14:17:44 +0000

    less

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 )
 
 func main() {
-   var firstNumb, secondNumb int
-   fmt.Scan(&firstNumb,&secondNumb)
-   var a int
+   a := 73081
+   b := 650
+   var result string
 
-   for firstNumb > 0 {
-      a = firstNumb % 10
+   numb := fmt.Sprintf("%d", a)
+   numb2 := fmt.Sprintf("%d", b)
+
+   fmt.Println(numb, " ", numb2)
+
+   for i:=0; i<len(numb);i++{
+      for k:=0; k<len(numb2);k++{
+         if string(numb[i]) == string(numb2[k]){
+            result += string(numb[i])
+         }
+      }
    }
-   fmt.Print(a)
+   fmt.Printf(result)
+
 }
    
