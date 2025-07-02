96ddfd16990a1ffac6f0eee789fb31f9f81c49d1
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-02 12:25:49 +0000

    test

--- main.go
+++ main.go
@@ -7,15 +7,11 @@ import (
 func main() {
    var firstNumb, secondNumb int
    fmt.Scan(&firstNumb,&secondNumb)
+   var a int
 
-   slice := make([]int, firstNumb)
-   slice2 := make([]int, secondNumb)
-
-   for i := 0; i < len(slice); i++ {
-      fmt.Println(slice[i])
+   for firstNumb > 0 {
+      a = firstNumb % 10
    }
-   for i := 0; i < len(slice2); i++ {
-      fmt.Println(slice2[i])
-   }
+   fmt.Print(a)
 }
    
