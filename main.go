7c82e4988a5488a3eba4a57ca5c1a855e2c082b9
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-03 08:21:04 +0000

    test

--- main.go
+++ main.go
@@ -4,35 +4,13 @@ import (
 	"fmt"
 )
 
-func main() {
-   var firstNumber int
-   var secondNumber int
+func main(){
+   var a int
+   fmt.Scan(&a)
 
-   fmt.Scan(&firstNumber, &secondNumber)
+   var nums []int
 
-   var result string
-   isDublicate := false
+   nums = append(nums, a)
 
-   numb := fmt.Sprintf("%d", firstNumber)
-   numb2 := fmt.Sprintf("%d", secondNumber)
-
-   fmt.Println(numb, " ", numb2)
-
-   for i:=0; i<len(numb);i++{
-      for k:=0; k<len(numb2);k++{
-         if numb[i] == numb2[k]{
-            for j := 0; j<len(result); j++{
-               if numb[i] == result[j]{
-                  isDublicate = true
-                  break
-               }
-            }
-            if !isDublicate{
-               result += string(numb[i])
-               result += " "
-            }
-         }
-      }
-   }
-   fmt.Print(result)
+   fmt.Print(nums)
 }
