8beea35e174c755e47c09ea49239227405c6eb1b
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-04 05:42:17 +0000

    done

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 )
 
 func main(){
-   var a int
-   fmt.Scan(&a)
+   var a, b int
+   fmt.Scan(&a,&b)
 
    var nums []int
+   var nums2 []int
+   var dublicateNums []int
 
    for a != 0 {
       value := a % 10
@@ -16,9 +18,23 @@ func main(){
       a /= 10
    }
 
-   fmt.Println(nums, " ")
+   for b != 0{
+      value := b%10
+      nums2 = append(nums2, value)
+      b /= 10
+   }
 
-   for i := len(nums); i > len(nums); i-- {
-      fmt.Println(nums[i])
+   fmt.Println(nums, " ")
+   fmt.Println(nums2, " ")
+
+   for i := 0; i < len(nums); i++ {
+      for j := 0; j < len(nums2); j++ {
+         if nums[i] == nums2[j] {
+            dublicateNums = append(dublicateNums, nums[i])
+         }
+      }
+   }
+   for i := len(dublicateNums)-1; i >= 0; i-- {
+      fmt.Print(dublicateNums[i], " ")
    }
 }
