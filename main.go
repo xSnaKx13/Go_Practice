3234cb77b27c3b9aa2141ca61a34e041681f941a
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-07 07:52:47 +0000

    less

--- main.go
+++ main.go
@@ -5,50 +5,19 @@ import (
 )
 
 func main(){
-   var a, b int
-   fmt.Scan(&a,&b)
-
-   var nums []int
-   var nums2 []int
-   var dublicateNums []int
-
-   for a != 0 {
-      value := a % 10
-      nums = append(nums, value)
-      a /= 10
-   }
-
-   for b != 0{
-      value := b%10
-      nums2 = append(nums2, value)
-      b /= 10
-   }
-   for i := len(nums)-1; i >= 0; i-- {
-      fmt.Print(nums[i])
-   }
-   fmt.Print(" ")
-   for i := len(nums2)-1; i >= 0; i-- {
-      fmt.Print(nums2[i])
-   }
-
-   for i := 0; i < len(nums); i++ {
-      for j := 0; j < len(nums2); j++ {
-         isDublicateValues := false
-         if nums[i] == nums2[j]{
-            for k := 0; k < len(dublicateNums); k++ {
-               if nums[i] == dublicateNums[k]{
-                  isDublicateValues = true
-                  break
-               }
-            }
-            if !isDublicateValues{
-               dublicateNums = append(dublicateNums, nums[i])
-            }
-         }
+   minimumFromFour()
+}
+func minimumFromFour() int{
+   var minValue int
+   var number int
+   fmt.Scan(&number)
+   minValue = number
+   for i := 1; i < 4; i++ {
+      fmt.Scan(&number)
+      if number < minValue{
+         minValue = number
       }
    }
-   fmt.Println()
-   for i := len(dublicateNums)-1; i >= 0; i-- {
-      fmt.Print(dublicateNums[i], " ")
-   }
+   fmt.Print(minValue)
+   return minValue
 }
