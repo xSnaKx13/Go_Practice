2b5801b063d4a6a16c6765954125cc03c8d96d8b
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-15 07:28:17 +0000

    less

--- main.go
+++ main.go
@@ -2,42 +2,17 @@ package main
 
 import (
 	"fmt"
+	"unicode"
 )
 
-type Cars struct{
-  _brand string
-  _model string
-  _horsePower float64
-  _yearRealize int
-}
-
-func NewCar(
-  brand string,
-  model string,
-  horsePower float64,
-  yearRealize int,
-) Cars {
-  if brand == "" {
-    return Cars{}
-  }
-  if model == "" {
-    return Cars{}
-  }
-  if horsePower < 70 || horsePower > 560 {
-    return Cars{}
-  }
-  if yearRealize < 1990 || yearRealize > 2025{
-    return Cars{}
-  }
-  return Cars{
-    _brand: brand,
-    _model: model,
-    _horsePower: horsePower,
-    _yearRealize: yearRealize,
-  }
-}
-
 func main(){
-  audi := NewCar("audi", "a4", 129.0, 2021)
-  fmt.Println(audi)
+  str := "Правило никто не отменял!"
+  
+  for _, elem := range str{
+    if unicode.IsUpper(elem[0]){
+      fmt.Println("да")
+    }else{
+      fmt.Println("нет")
+    }
+  }
 }
