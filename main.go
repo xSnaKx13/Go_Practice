ae4563b5a2183ff4174d51baca72b1f25d5673f8
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-15 07:38:55 +0000

    test

--- main.go
+++ main.go
@@ -2,17 +2,15 @@ package main
 
 import (
 	"fmt"
-	"unicode"
 )
 
 func main(){
-  str := "Правило никто не отменял!"
-  
-  for _, elem := range str{
-    if unicode.IsUpper(elem[0]){
-      fmt.Println("да")
-    }else{
-      fmt.Println("нет")
-    }
+  str := "правило никто не отменял!"
+
+  lastS := []rune(str)[len(str)]
+  if lastS == '!'{
+    fmt.Println("да")
+  }else{
+    fmt.Println("нет")
   }
 }
