b0426f6fb133ecd7858335187e413ad3263ac746
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-21 11:07:45 +0000

    less

--- main.go
+++ main.go
@@ -2,23 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"strings"
 )
 
 func main(){
-  var a, b int
-  _, err := fmt.Scan(&a, &b)
-  if err != nil{
-    fmt.Println("ошибка")
-    return
-  }
-  hypotenuse(a, b)
-}
-func hypotenuse(a,b int)int{
-    squareA := a * a
-    squareB := b * b
-    sumAB := squareA + squareB
-    hptnse := math.Sqrt(float64(sumAB))
-    fmt.Println(int(hptnse))
-    return int(hptnse)
+  var s string = "qwerty"
+  newS := strings.Split(s, "")
+  result := strings.Join(newS, "*")
+  fmt.Println(result)
 }
