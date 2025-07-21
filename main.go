740da302b3610b937a283b980497bca70792cdaa
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-21 08:23:34 +0000

    test

--- main.go
+++ main.go
@@ -5,16 +5,17 @@ import (
 )
 
 func main(){
-  devide(10,2)
+  var a, b int
+  _, err := fmt.Scan(&a, &b)
+  if err != nil{
+    fmt.Println("ошибка")
+    return
+  }
+
+  res, err := devide(a, b)
+  fmt.Println(res)
 }
 
 func devide(a,b int) (int, error){
-  fmt.Println("Введите два целых числа:")
-  _,error := fmt.Scan(&a, &b)
-  if error != nil{
-    error = fmt.Println("ошибка")
-  }else{
-    devide(a, b)
-  }
-  return error, a/b
+  return a/b, nil
 }
