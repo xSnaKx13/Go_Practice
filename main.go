4397e64a71100aa00002087ecdc36b397c27eece
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-22 06:35:28 +0000

    less

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"slices"
 	"strings"
 )
 
@@ -12,17 +12,6 @@ func main(){
   var input string
   input, _ = bufio.NewReader(os.Stdin).ReadString('\n')
   input = strings.TrimSpace(input)
-
-  var numbers[] int
-  for i := 0; i < len(input); i++ {
-    s, _:= strconv.Atoi(string(input[i]))
-    numbers = append(numbers, s)
-  }
-  var maxNumber int = 0
-  for _, elem := range numbers {
-    if elem > maxNumber{
-      maxNumber = elem
-    }
-  }
-  fmt.Println(maxNumber)
+  numbers := []rune(input)
+  fmt.Println(string(slices.Max(numbers)))
 }
