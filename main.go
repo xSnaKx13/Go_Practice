ae7302d58ad19d9b2e78df636e7bfdb13d969513
Ваше имя <152309157+xSnaKx13@users.noreply.github.com>
2025-07-23 14:02:23 +0000

    test

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main(){
-  v := 5
-  p := &v
-  
-  fmt.Print(*p, " ")
-  changePointer(p)
-  fmt.Println(v)
-  fmt.Println("testttt")
-}
-func changePointer(j *int){
-  v := 3
-  j = &v
+  var weight float64 
+  var height float64 
+  fmt.Print("Введите ваш вес: ")
+  fmt.Scan(&weight)
+  fmt.Print("Введите ваш рост: ")
+  fmt.Scan(&height)
+  IMT := weight / math.Pow(height, 2)
+
+  fmt.Printf("Ваш ИМТ: %.2f", IMT)
 }
